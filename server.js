d810c17c75426990c28a43a1823ac734928f0b67
Michał Górniewski <112617853+mgorniew-carma@users.noreply.github.com>
2025-07-10 13:57:50 +0200

    Add support for Grafana service accounts

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const { fork } = require('child_process');
 
 const GRAFANA_USER = process.env.GRAFANA_USER;
 const GRAFANA_PASSWORD = process.env.GRAFANA_PASSWORD;
+const GRAFANA_SERVICE_ACCOUNT = process.env.GRAFANA_SERVICE_ACCOUNT;
 
 const app = express();
 const port = process.env.EXPORT_SERVER_PORT || 3001;
@@ -40,10 +41,11 @@ app.post('/generate-pdf', (req, res) => {
 
   const finalUrl = urlObj.toString();
 
-  const args = [finalUrl, `${GRAFANA_USER}:${GRAFANA_PASSWORD}`];
+  const args = [finalUrl, GRAFANA_SERVICE_ACCOUNT ? `${GRAFANA_PASSWORD}` : `${GRAFANA_USER}:${GRAFANA_PASSWORD}`];
 
   if (pdfWidthPx) args.push(`--pdfWidthPx=${pdfWidthPx}`);
   if (pdfHeightPx) args.push(`--pdfHeightPx=${pdfHeightPx}`);
+  if (GRAFANA_SERVICE_ACCOUNT === 'true') args.push('--serviceAccount');
 
   const script = fork('grafana_pdf.js', args);
 
